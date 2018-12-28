a673bcb0f29359d78163dd320af7e8e9fa4cbb42
Shyam Kumar <sykron@gmail.com>
2018-12-28 23:21:29 +0800

    Heroku Deploy

---
+++ public/precache-manifest.550bfbb4e6484bbc08aea6a578028e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34c88a70f6aaa2c5e46f",
+    "url": "/static/js/main.34c88a70.chunk.js"
+  },
+  {
+    "revision": "8281ddd7863c6fcccf8e",
+    "url": "/static/js/1.8281ddd7.chunk.js"
+  },
+  {
+    "revision": "34c88a70f6aaa2c5e46f",
+    "url": "/static/css/main.e630c9a8.chunk.css"
+  },
+  {
+    "revision": "3622b5ab4c2eaa01b1c541af700d3137",
+    "url": "/index.html"
+  }
+];
